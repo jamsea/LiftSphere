500a763c4b65c68d0bc1f8c4be54387251ca9a43
Jamie Hush <jhush91@gmail.com>
2012-06-08 12:59:12 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiftSphere::Application.config.secret_token = '6d59ba55bffe1d94b286cad7df6540c5df440025b2cb688b18799238badbdc30fa8f4c08726285a0a67f6bb5cf7dd7bb48b862a4649aa7503716289c387f55ed'
