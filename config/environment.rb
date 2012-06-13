a4bbb8b9e2cdc8c1805c338d36538e5f0bc9e11a
Jamie Hush <jhush91@gmail.com>
2012-06-12 20:05:36 -0400

    Changed global timezone

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
-config.time_zone = "Eastern Time (US & Canada)"
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 LiftSphere::Application.initialize!
+
+config.time_zone = "Eastern Time (US & Canada)"
