d2033272347a23a2f6365a6713ec2aa8f1cd89c9
Jamie Hush <jhush91@gmail.com>
2012-06-12 20:45:35 -0400

    Changed global timezone

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -1,7 +1,8 @@
 class ActiveSupport::TimeWithZone
     def as_json(options = {})
         Time.zone = 'Eastern Time (US & Canada)'
-        strftime('%Y-%m-%d %H:%M:%S')
+        strftime('%Y-%m-%d %H:%M:%S').in_time_zone("Eastern Time (US & Canada)")
+
     end
 end
 
