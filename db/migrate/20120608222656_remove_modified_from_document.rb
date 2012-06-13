06207dffead7af42cbff820c92c2eb932c6911f1
Jamie Hush <jhush91@gmail.com>
2012-06-13 12:22:56 -0400

    Added modified field to Documents.

--- db/migrate/20120608222656_remove_modified_from_document.rb
+++
@@ -1,9 +0,0 @@
-class RemoveModifiedFromDocument < ActiveRecord::Migration
-  def up
-    remove_column :documents, :modified
-      end
-
-  def down
-    add_column :documents, :modified, :date
-  end
-end
